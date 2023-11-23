c70394f845b38c40c7ef0e6ad1088e93073f8e97
lbarbier22 <barbier.lucas08@gmail.com>
2023-11-23 08:55:12 +0100

    add services in ServiceServiceImpl

--- src/main/java/fr/efficom/appweb/service/ServiceService.java
+++ src/main/java/fr/efficom/appweb/service/ServiceService.java
@@ -1,4 +1,9 @@
 package fr.efficom.appweb.service;
 
+import fr.efficom.appweb.pojo.Chips;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public interface ServiceService {
+
+    Chips getChipsByID(String id);
 }
--- src/main/java/fr/efficom/appweb/service/impl/ServiceServiceImpl.java
+++ src/main/java/fr/efficom/appweb/service/impl/ServiceServiceImpl.java
@@ -1,8 +1,23 @@
 package fr.efficom.appweb.service.impl;
 
+import fr.efficom.appweb.pojo.Chips;
+import fr.efficom.appweb.repository.ChipsRepository;
 import fr.efficom.appweb.service.ServiceService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ServiceServiceImpl implements ServiceService {
+
+    @Autowired
+    private ChipsRepository chipsRepository;
+
+    @Override
+    public Chips getChipsByID(String id) {
+        return chipsRepository.findById(id).orElse(null);
+    }
+
+    public void saveChips(Chips chips){
+        chipsRepository.save(chips);
+    }
 }
