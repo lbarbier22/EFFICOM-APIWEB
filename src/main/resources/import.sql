69316f161c0cb829560bddff8b0f8b1f963fb6a6
lbarbier22 <barbier.lucas08@gmail.com>
2023-12-06 22:56:15 +0100

    tg

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO Note(nom,reproductionGout,originalite,addictivite,ressenti,marque,bonus,note) VALUES ('Sauce Pomme Frites','4','2,75','5','5,5','Brets','1','18,25');
+INSERT INTO Note(nom,reproductionGout,originalite,addictivite,ressenti,marque,bonus,note) VALUES ('Wavy BBQ','3','2','5,5','6,5','Pringles','0,5','17,5');
+INSERT INTO Note(nom,reproductionGout,originalite,addictivite,ressenti,marque,bonus,note) VALUES ('Miel Moutarde','3,75','1,5','5','6,25','Brets','0,5','17');
+INSERT INTO Chips(nom,marque) VALUES ('blabla','Brets')
