80f7f5ea8a0426d25f6b41c40652e0a595e2b5d3
Algamor59 <algamor59@gmail.com>
2023-12-18 10:12:22 +0100

    En despi j'ai réussi la bdd ahaha

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO chips(marque,nom) VALUES ('Brets','Sauce Pomme Frites')
+INSERT INTO note(addictivite,bonus,note,originalite,reproduction_gout,ressent) VALUES ('5','1','18,25','2,75','4','5,5')
